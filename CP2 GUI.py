e5755fa0f2716cc527ba498ecd7352d6a15255b3
cyrusdacup3-commits <cyrusdacup3@gmail.com>
2026-05-25 21:46:54 +0800

    added window and background color

--- CP2 GUI.py
+++ CP2 GUI.py
@@ -1,1 +1,13 @@
-commit mo tanan
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+
+window = tk.Tk()
+window.title("Student Management System")
+window.geometry("950x600")
+window.config(bg="#16161d")
+
+bg_color = "#16161d"
+card_color = "#232333"
+button_color = "#7c5cff"
+text_color = "white"
